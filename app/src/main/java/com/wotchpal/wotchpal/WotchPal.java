7425a3d7e9b8ee87c1adc5c2f06ffc66a2ccfcfa
Alex Foster <pair+alexjfno1@gmail.com>
2015-01-07 23:36:27 +0000

    Error message and try again button when web view fails to load.

--- app/src/main/java/com/wotchpal/wotchpal/WotchPal.java
+++ app/src/main/java/com/wotchpal/wotchpal/WotchPal.java
@@ -3,7 +3,10 @@ package com.wotchpal.wotchpal;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
 
 
 public class WotchPal extends Activity {
@@ -12,10 +15,31 @@ public class WotchPal extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        WebView webview = new WebView(this);
+
+        final WebView webview = new WebView(this);
         webview.getSettings().setJavaScriptEnabled(true);
+
+        webview.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedError(WebView webView, int errorCode, String description, String failingUrl) {
+                setContentView(R.layout.wotch_pal_error);
+                setTryAgainClickListener();
+            }
+        });
+
         webview.loadUrl("http://wotchpal.com");
         setContentView(webview);
+    }
+
+    private void setTryAgainClickListener() {
+        Button tryAgain = (Button) findViewById(R.id.try_again);
+        tryAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                startActivity(getIntent());
+            }
+        });
     }
 
 }
